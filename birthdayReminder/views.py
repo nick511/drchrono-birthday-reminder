822d4adbfa82209649e4f635cca602c078599b1d
Nick Hu <nick@511.pm>
2016-03-20 14:59:09 -0700

    update views.py

--- birthdayReminder/views.py
+++ birthdayReminder/views.py
@@ -4,19 +4,25 @@ from django.shortcuts import render
 from django.conf import settings
 
 from .scripts.drchrono import DrChronoClient
-
+from .models import Patient
+from .serializers import PatientSerializer
 
 
 def index(request):
 
   client = DrChronoClient(settings.CLIENT_ID, settings.CLIENT_SECRET, settings.REDIRECT_URI)
 
-  code = request.GET.get('code')
-  if code:
-    request.session['token'] = client.get_token(code)
 
   if request.session.has_key('token'):
-    client.get_patients_summary(request.session.get('token'))
+    patients = client.get_patients(request.session.get('token'))
+
+    for patient in patients:
+      PatientSerializer.update_from_data(patient)
+
+  else:
+    code = request.GET.get('code')
+    if code:
+      request.session['token'] = client.get_token(code)
 
 
   vm = {
